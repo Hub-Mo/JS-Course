27e74543bb841ac03405fe6a2a0a57dcbe4f4d7f
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-05 20:19:19 +0200

    finished locale-date

--- js-complete-course/03-dates/04-locale-date-Optional/script.js
+++ js-complete-course/03-dates/04-locale-date-Optional/script.js
@@ -11,6 +11,63 @@
 
 (function() {
 
+
+const d = new Date();
+let day = d.getDate();
+let month = d.getMonth() + 1;
+const year = d.getFullYear();
+const hour = d.getHours();
+const minut = d.getMinutes();
+
+
+if (month == 1){
+    month = "January"
+}
+else if (month == 2){
+    month = "February"
+}
+else if (month == 3){
+    month = "March"
+}
+else if (month == 4){
+    month = "april"
+}
+else if (month == 5){
+    month = "May"
+}
+else if (month == 6){
+    month = "June"
+}
+else if (month == 7){
+    month = "July"
+}
+else if (month == 8){
+    month = "August"
+}
+else if (month == 9){
+    month = "Septamber"
+}
+else if (month == 10){
+    month = "October"
+}
+else if (month == 11){
+    month = "November"
+}
+else if(month == 12) {
+    month = "December"
+}
+else {
+    alert("wrong month");
+}
+
+function getDayName(day, locale){
+    let dn = new Date(day);
+    return dn.toLocaleDateString(locale, { weekday: 'long'})
+}
+let dayName = getDayName(day, "en-US");
+
+document.getElementById('target').innerHTML = `${`${dayName} ${day} ${month} ${year} ${hour}`}h${minut}`;
+
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
