98fa641634834f8e446d7fb1428a75ef44f4dfda
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-12 16:46:16 +0200

    finished 07-fetch/03-list-to-template-Optional added a forEach and created element where i appended the right hero data

--- 07-fetch/03-list-to-template-Optional/script.js
+++ 07-fetch/03-list-to-template-Optional/script.js
@@ -10,5 +10,28 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', magic)
+
+    async function magic() {
+
+        const Data = await fetch('../../_shared/api.json')
+        const dataContent = await Data.json();
+
+        let target = document.getElementById('target');
+        dataContent.heroes.forEach(item => {
+            let l1 = document.createElement('li');
+                let h4 = document.createElement('h4');
+                let pTag = document.createElement('p');
+                let em = document.createElement('em');
+                target.append(l1);
+                target.append(pTag);
+                l1.append(h4);
+                l1.append(em);
+                h4.innerHTML = item.name;
+                pTag.innerHTML = item.abilities;
+                em.innerHTML = item.alterEgo;
+        })
+        
+
+    }
 })();
